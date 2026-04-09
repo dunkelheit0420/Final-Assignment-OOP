d2dac6e3e188db33480b70cc4cf5596c53f6579b
dunkelheit0420 <dgon_001@yahoo.com.ph>
2026-04-09 17:41:22 -0400

    Student Class almost completed

--- library.cpp
+++ library.cpp
@@ -6,7 +6,9 @@ Person::Person() : full_name_(""), birthday_(""), gender_(other) {}
 Person::Person(const std::string& full_name, const std:: string& birthday, gender gender) :
     full_name_(full_name), birthday_(birthday), gender_(gender) {}
 
-Person::~Person() {}
+Person::~Person() {
+    std::cout << full_name_ << " is deleted." << std::endl;
+}
 
 void Person::display() const {
     std::cout << full_name_ << ", " << birthday_ << ", " << gender_;
@@ -19,32 +21,28 @@ std::string Person::name() const {
 
 //Student class functions implementation
 
-Student::Student(){
+Student::Student() : Person(), gpa_(0.0) {}
 
+Student::Student(const std::string& full_name, const std::string& birthday, gender gender, double gpa) : 
+    Person(full_name, birthday, gender), gpa_(gpa) {}
+
+Student::Student(const Student& other): Person(other), gpa_(other.gpa_){}
+
+Student::~Student() {
+    std::cout << "Student ";
 }
 
-Student::Student(const std::string&, const std::string&, gender, double){
-
-}
-
-Student::Student(const Student&){
-
-}
-
-Student::~Student(){
-
-}
-
-void Student::display() const{
-
+void Student::display() const {
+    Person::display();
+    std::cout << " " << gpa_ << std::endl;
 }
 
 std::string Student::name() const{
-
+    return Person::name();
 }
 
 void Student::add_course(const Course&){
-
+    
 }
 
 
